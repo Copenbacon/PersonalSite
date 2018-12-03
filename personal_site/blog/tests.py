cda8da95e89b05a1d16b8bf1875730e90b977221
copenbacon <sclary50@gmail.com>
2018-12-03 11:17:09 -0800

    tests and gitignore

--- personal_site/blog/tests.py
+++ personal_site/blog/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+"""Test the Entry and Entry Album Models."""
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse_lazy
+from .models import Entry, Album
+import factory
+
