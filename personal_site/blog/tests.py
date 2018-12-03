4e190fbdab028f10278874fffdd727a7b7254c1d
copenbacon <sclary50@gmail.com>
2018-12-03 12:41:55 -0800

    testing some models

--- personal_site/blog/tests.py
+++ personal_site/blog/tests.py
@@ -6,33 +6,39 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse_lazy
-from .models import Entry, Album
+from blog.models import Entry, Album
+from blogprofile.models import BlogProfile
 import factory
+
 
 class UserFactory(factory.django.DjangoModelFactory):
 	"""Create a bunch of fake users."""
+
 	class Meta:
 		model = User
-		username = factory.Sequence(lambda n: "Blog User {}".format(n))
-		email = factory.LazyAttribute(
-		lambda x: "{}@datsite.com".format(x.username.replace(" ", ""))
-    )
+	username = factory.Sequence(lambda n: "Blog User {}".format(n))
+	email = factory.LazyAttribute(lambda x: "{}@datsite.com".format(x.username.replace(" ", ""))
+	)
+
 
 class EntryFactory(factory.django.DjangoModelFactory):
 	"""Create a bunch of fake entries."""
+
 	class Meta:
 		model = Entry
-		title = factory.Sequence(lambda n: "Entry {}".format(n))
-		image_file = SimpleUploadedFile(name='image_1.jpg', content=open('imagersite/static/images/image_1.jpg', 'rb').read(), content_type='image/jpeg')
+	title = factory.Sequence(lambda n: "Entry {}".format(n))
+	image_file = SimpleUploadedFile(name='image_1.jpg', content=open('static/images/image_1.jpg', 'rb').read(), content_type='image/jpeg')
 
 
 class AlbumFactory(factory.django.DjangoModelFactory):
-	"""Create a bunch of fake Albums"""
+	"""Create a bunch of fake Albums."""
+
 	class Meta:
 		model = Album
-		title = factory.Sequence(lambda n: "Album {}".format(n))
-		cover_image = SimpleUploadedFile(name='image_1.jpg', content=open('imagersite/static/images/image_1.jpg', 'rb').read(), content_type='image/jpeg')
-		description = "Some Album"
+	title = factory.Sequence(lambda n: "Album {}".format(n))
+	cover_image = SimpleUploadedFile(name='image_1.jpg', content=open('static/images/image_1.jpg', 'rb').read(), content_type='image/jpeg')
+	description = "Some Album"
+
 
 class EntryTestCase(TestCase):
 	"""Entry Test Class."""
@@ -46,3 +52,10 @@ class EntryTestCase(TestCase):
 	def test_entry_title(self):
 		"""Test that entry has a title."""
 		self.assertTrue("Entry" in Entry.objects.first().title)
+
+	def test_image_has_description(self):
+		"""Test that the Entry description field can be assigned."""
+		entry = Entry.objects.first()
+		entry.description = "This is a good entry."
+		entry.save()
+		self.assertTrue(Entry.objects.first().description == "This is a good entry.")
