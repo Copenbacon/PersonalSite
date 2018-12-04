8a209944930196328479a05fb322ed826c3877d3
copenbacon <sclary50@gmail.com>
2018-12-03 19:50:06 -0800

    more tests

--- personal_site/blog/tests.py
+++ personal_site/blog/tests.py
@@ -53,9 +53,38 @@ class EntryTestCase(TestCase):
 		"""Test that entry has a title."""
 		self.assertTrue("Entry" in Entry.objects.first().title)
 
-	def test_image_has_description(self):
+	def test_entry_has_description(self):
 		"""Test that the Entry description field can be assigned."""
 		entry = Entry.objects.first()
 		entry.description = "This is a good entry."
 		entry.save()
 		self.assertTrue(Entry.objects.first().description == "This is a good entry.")
+
+	def test_entry_has_published(self):
+		"""Test the entry published field."""
+		entry = Entry.objects.first()
+		entry.published = 'public'
+		entry.save()
+		self.assertTrue(Entry.objects.first().published == "public")
+
+	def test_entry_has_no_author(self):
+		"""Test the entry instantiates with no author."""
+		entry = Entry.objects.first()
+		entry.save()
+		self.assertFalse(entry.author)
+
+	def test_entry_has_author(self):
+		"""Test the entry can be assigned an author."""
+		entry = Entry.objects.first()
+		user1 = User.objects.first()
+		entry.author = user1
+		entry.save()
+		self.assertTrue(entry.author)
+
+	def test_author_has_entry(self):
+		"""Test the entry published field."""
+		entry1 = Entry.objects.first()
+		user1 = User.objects.first()
+		entry1.author = user1
+		entry1.save()
+		self.assertTrue(user1.profile == entry1.author.profile)
